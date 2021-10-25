6fee31de4eab8bbe2928c20d8663d36457593f90
Holvert Josué Pérez Talavera <50637750+Holvert-P@users.noreply.github.com>
2021-10-24 23:37:42 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const animation = document.querySelector('.navbar');
+const button = document.querySelector('.container__button');
+const navbar__menu = document.querySelector('.container__menu');
+const button__search = document.querySelector('.search__button');
+
+button.addEventListener('click', () => {
+    animation.classList.toggle('navbar__animation');
+    navbar__menu.classList.toggle('animation__menu');
+    button.classList.toggle('animation__button')
+    button__search.classList.toggle('search__button--toggle')
+})
+
+button.addEventListener("blur", function(event) {
+    animation.classList.toggle('navbar__animation');
+    navbar__menu.classList.toggle('animation__menu');
+    button.classList.toggle('animation__button')
+    button__search.classList.toggle('search__button--toggle')
+}, true);
